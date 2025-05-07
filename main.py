ffc67d77449d10282a51a9a11d88407bbf64cd7d
Nelly <nellyperez1111@gmail.com>
2025-05-07 11:41:25 +0200

    dodal scraper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from recipe_scrapers import scrape_me
+import re
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -22,6 +24,36 @@ class User(db.Model):
 @app.route('/')
 def index():
     return render_template("home.html")
+
+
+
+url_pattern = re.compile(
+    r'^(?:http|ftp)s?://'
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}|'
+    r'localhost|'
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    r'(?::\d+)?'
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+
+@app.route('/scrape', methods=['POST'])
+def scrape(url=None):
+  url = request.form['url'] if url is None else url
+
+  if not url or not re.match(url_pattern, url):
+    return render_template('index.html', message='Invalid URL')
+
+  scraper = scrape_me(url)
+  recipe_data = {
+      'title': scraper.title(),
+      'url': url,
+      'ingredients': scraper.ingredients(),
+      'instructions': scraper.instructions(),
+      'image_url': scraper.image(),
+  }
+
+  return render_template('scraper.html')
+
 
 # Main entry point
 if __name__ == "__main__":
