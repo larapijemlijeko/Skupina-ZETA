3e57279cc0964d94a2301b687d72e4675ff3d555
Matevž Kovačič <matevz.kovacic1@student.um.si>
2025-05-28 18:32:43 +0200

    spremenjena tabela za vsečkanje

--- src/models/vsecki.py
+++ src/models/vsecki.py
@@ -2,9 +2,7 @@ def create_table(cur):
     cur.execute("""
         CREATE TABLE IF NOT EXISTS vsecki (
             id SERIAL PRIMARY KEY,
-            uporabnik_id INTEGER NOT NULL REFERENCES uporabniki(id) ON DELETE CASCADE,
             recept_id INTEGER NOT NULL REFERENCES recepti(id) ON DELETE CASCADE,
-            datum_vsecka TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            UNIQUE (uporabnik_id, recept_id)
+            like BOOLEAN NOT NULL,  -- True = like, False = dislike
         );
     """)
