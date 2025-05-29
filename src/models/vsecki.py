3071e923e88b451b1f995a656cc24e83c2db8a1b
Vid <vid.strojan@student.um.si>
2025-05-29 11:15:10 +0200

    fixana baza vsecki

--- src/models/vsecki.py
+++ src/models/vsecki.py
@@ -3,6 +3,6 @@ def create_table(cur):
         CREATE TABLE IF NOT EXISTS vsecki (
             id SERIAL PRIMARY KEY,
             recept_id INTEGER NOT NULL REFERENCES recepti(id) ON DELETE CASCADE,
-            like BOOLEAN NOT NULL,  -- True = like, False = dislike
+            vsecek BOOLEAN NOT NULL
         );
     """)
